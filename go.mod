490aef2c76f04142ddf440cc21a9503fef6d7ea5
KrishanBhalla <Krishanbhalla1@gmail.com>
2021-01-26 22:25:16 +0000

    slack service

--- go.mod
+++ go.mod
@@ -5,9 +5,9 @@ go 1.15
 require (
 	github.com/bwmarrin/discordgo v0.23.0
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
 	github.com/pkg/errors v0.9.1
+	github.com/slack-go/slack v0.8.0
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20190605123033-f99c8df09eb5 // indirect
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
---
+++ service/slack/slack.go
@@ -0,0 +1,50 @@
+package telegram
+
+import (
+	"github.com/pkg/errors"
+	"github.com/slack-go/slack"
+)
+
+// Slack struct holds necessary data to communicate with the Slack API.
+type Slack struct {
+	client     *slack.Client
+	channelIDs []string
+}
+
+// New returns a new instance of a Slack notification service.
+// For more information about slack api token:
+//    -> https://pkg.go.dev/github.com/slack-go/slack#New
+func New(apiToken string) (*Slack, error) {
+	client := slack.New(apiToken)
+
+	s := &Slack{
+		client:     client,
+		channelIDs: []string{},
+	}
+
+	return s, nil
+}
+
+// AddReceivers takes Slack channel IDs and adds them to the internal channel ID list. The Send method will send
+// a given message to all those channels.
+func (s *Slack) AddReceivers(channelIDs ...string) {
+	s.channelIDs = append(s.channelIDs, channelIDs...)
+}
+
+// Send takes a message subject and a message body and sends them to all previously set channels.
+func (s Slack) Send(subject, message string) error {
+	fullMessage := subject + "\n" + message // Treating subject as message title
+
+	for _, channelID := range s.channelIDs {
+
+		id, timestamp, err := s.client.PostMessage(
+			channelID,
+			slack.MsgOptionText(fullMessage, false),
+		)
+		if err != nil {
+			return errors.Wrapf(err, "failed to send message to Slack channel '%d' at time '%s'", id, timestamp)
+		}
+	}
+
+	return nil
+}
