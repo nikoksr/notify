0331a291e176bd6202f2654615474795a38e734d
RandoomJD <james.dolan@liverpool.ac.uk>
2021-10-24 00:23:19 +0000

    Add webpush service

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Baozisoftware/qrcode-terminal-go v0.0.0-20170407111555-c0650d8dff0f
 	github.com/Rhymen/go-whatsapp v0.1.1
 	github.com/RocketChat/Rocket.Chat.Go.SDK v0.0.0-20210506200512-3b3352a919f0
+	github.com/SherClockHolmes/webpush-go v1.1.3
 	github.com/antihax/optional v1.0.0 // indirect
 	github.com/atc0005/go-teams-notify/v2 v2.6.0
 	github.com/aws/aws-sdk-go-v2 v1.9.1
---
+++ service/webpush/webpush.go
@@ -0,0 +1,72 @@
+package webpush
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	webpush "github.com/SherClockHolmes/webpush-go"
+	"github.com/pkg/errors"
+)
+
+type Service struct {
+	opts        webpush.Options
+	subscribers []webpush.Subscription
+}
+
+// New returns a new instance of a Web Push notification service.
+// It takes an email, for subscribers to respond to push notifications,
+// together with a public/private key pair to encrypt notifications.
+func New(email, publicKey, privateKey string) *Service {
+	return &Service{
+		opts: webpush.Options{
+			Subscriber:      email,
+			VAPIDPrivateKey: privateKey,
+			VAPIDPublicKey:  publicKey,
+		},
+	}
+}
+
+// SubService creates a new subservice with a given topic and urgency.
+// Notifications of the same topic will overwrite eachother.
+// If urgent the notification will be marked high urgency.
+
+func (s Service) SubService(topic string, urgent bool) *Service {
+	s.opts.Topic = topic
+	if urgent {
+		s.opts.Urgency = webpush.UrgencyHigh
+	}
+	return &s
+}
+
+// AddRecievers takes a json response body from a subscription, unmarshals it,
+// and adds it to the list of subscribers.
+func (s *Service) AddReceivers(subscribers ...string) {
+	for _, sub := range subscribers {
+		subscriber := webpush.Subscription{}
+		err := json.Unmarshal([]byte(sub), &subscriber)
+		if err != nil {
+			log.Printf("Cound not add subscriber: %v\n%s", err, subscriber)
+			continue
+		}
+		s.subscribers = append(s.subscribers, subscriber)
+	}
+}
+
+func (s *Service) Send(ctx context.Context, subject, message string) error {
+	msg := []byte(subject + "\n" + message)
+
+	for _, subscriber := range s.subscribers {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			resp, err := webpush.SendNotification(msg, &subscriber, &s.opts)
+			if err != nil {
+				return errors.Wrapf(err, "failed to send message to Web Push subscriber '%s'\nStatus: %s", subscriber, resp.Status)
+			}
+		}
+	}
+
+	return nil
+}
