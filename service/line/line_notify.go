a15ae14b53a75d4ab0069845390428097d89401f
Panapol Poungdech <boom.rst28@gmail.com>
2022-02-06 20:36:09 +0700

    [fix] lint error

--- service/line/line_notify.go
+++ service/line/line_notify.go
@@ -2,12 +2,13 @@ package line
 
 import (
 	"context"
+
 	"github.com/pkg/errors"
 	"github.com/utahta/go-linenotify"
 )
 
 // Line Notify struct holds info about client and destination token for communicating with line API
-type LineNotify struct {
+type Notify struct {
 	client         *linenotify.Client
 	receiverTokens []string
 }
@@ -15,22 +16,22 @@ type LineNotify struct {
 // New creates a new instance of Line notify service
 // For more info about line notify api:
 // -> https://notify-bot.line.me/doc/en/
-func NewNotify() *LineNotify {
+func NewNotify() *Notify {
 	c := linenotify.NewClient()
-	l := &LineNotify{
+	l := &Notify{
 		client: c,
 	}
 	return l
 }
 
 // AddReceivers receives token then add them to internal receivers list
-func (ln *LineNotify) AddReceivers(receiverTokens ...string) {
+func (ln *Notify) AddReceivers(receiverTokens ...string) {
 	ln.receiverTokens = append(ln.receiverTokens, receiverTokens...)
 }
 
 // Send receives message subject and body then sends it to all receivers set previously
 // Subject will be on the first line followed by message on the next line
-func (ln *LineNotify) Send(ctx context.Context, subject, message string) error {
+func (ln *Notify) Send(ctx context.Context, subject, message string) error {
 	lineMessage := subject + "\n" + message
 
 	for _, receiverToken := range ln.receiverTokens {
