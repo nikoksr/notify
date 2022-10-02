82c1b50e2878a65d16f6c60f500392cb960b61cf
Arno Cornette <arno@gazzell.be>
2022-10-02 08:53:10 +0200

    feat(service): implemented matrix service

---
+++ service/matrix/matrix_test.go
@@ -0,0 +1,37 @@
+package matrix
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	matrix "maunium.net/go/mautrix"
+	"maunium.net/go/mautrix/event"
+	"maunium.net/go/mautrix/id"
+	"testing"
+)
+
+func TestService_SetRoomID(t *testing.T) {
+	t.Parallel()
+	assert := require.New(t)
+	service, err := New("fake-user-id", "fake-home-server", "fake-home-server", "fake-access-token")
+	assert.Nil(err)
+	assert.NotNil(service)
+	assert.Equal(id.UserID("fake-user-id"), service.options.userID)
+	assert.Equal("fake-home-server", service.options.homeServer)
+	assert.Equal("fake-access-token", service.options.accessToken)
+}
+
+func TestService_Send(t *testing.T) {
+	t.Parallel()
+	assert := require.New(t)
+	// Test response
+	mockClient := newMockMatrixClient(t)
+	mockClient.
+		On("SendMessageEvent", id.RoomID("fake-room-id"), event.EventMessage, &Message{Body: "fake-message", Msgtype: event.MsgText}).Return(&matrix.RespSendEvent{}, nil)
+	service, _ := New("fake-user-id", "fake-room-id", "fake-home-server", "fake-access-token")
+	service.client = mockClient
+	err := service.Send(context.Background(), "", "fake-message")
+	assert.Nil(err)
+
+	mockClient.AssertExpectations(t)
+
+}
