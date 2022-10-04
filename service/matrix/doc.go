57c959c11c7596d4496ef9d4ae2bf73f36f4f014
Arno Cornette <arno@gazzell.be>
2022-10-04 13:21:45 +0200

    chore(formatting): Reformatted docs

--- service/matrix/doc.go
+++ service/matrix/doc.go
@@ -1,34 +1,33 @@
 /*
-Package matrix provides message notification integration for Matrix.
+Package matrix provides message notification integration for WhatsApp.
 
 Usage:
 
 	package main
 
-import (
+	import (
+		"log"
 
-	"context"
-	"log"
-	"github.com/nikoksr/notify"
-	"github.com/nikoksr/notify/service/matrix"
+		"github.com/nikoksr/notify"
+		"github.com/nikoksr/notify/service/matrix"
+	)
 
-)
+	func main() {
+		matrixSvc, err := matrix.New("fake-user-id", "fake-room-id", "fake-home-server", "fake-access-token")
 
-		func main() {
-		  matrixSvc, err := matrix.New("user-id", "room-id", "home-server", "access-token")
-		  if err != nil {
-		    log.Fatalf("matrix.New() failed: %s", err.Error())
-		  }
+		if err != nil {
+			log.Fatalf("matrix.New() failed: %s", err.Error())
+		}
 
-		  notifier := notify.New()
-		  notifier.UseServices(matrixSvc)
+		notifier := notify.New()
+		notifier.UseServices(matrixSvc)
 
-		  err = notifier.Send(context.Background(), "", "message")
-		  if err != nil {
-		    log.Fatalf("notifier.Send() failed: %s", err.Error())
-		  }
+		err = notifier.Send(context.Background(), "subject", "message")
+		if err != nil {
+			log.Fatalf("notifier.Send() failed: %s", err.Error())
+		}
 
-		  log.Println("notification sent")
-	    }
+		log.Println("notification sent")
+	}
 */
 package matrix
