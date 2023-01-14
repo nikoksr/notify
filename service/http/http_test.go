e31ceaed0a326bd00cb85a9a300e67855b029b65
Niko Köser <koeserniko@gmail.com>
2023-01-14 17:59:00 +0100

    test(http): add missing parallelization

--- service/http/http_test.go
+++ service/http/http_test.go
@@ -363,7 +363,9 @@ func TestWebhook_String(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			assert.Equalf(t, tt.want, tt.hook.String(), "String() = %v, want %v", tt.hook.String(), tt.want)
 		})
 	}
